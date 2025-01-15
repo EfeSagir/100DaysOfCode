5e74e1aa4c5e45d274b919c37b3db44919832199
Efe <efeserdar123123@gmail.com>
2025-01-15 21:01:55 +0300

    day7

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-
+import math
+import itertools
 # print("Hello world!")
 #
 # print("+Hello Efe How Are You? \n-I am fine hbu?")
@@ -374,5 +375,65 @@ import random
 #         move()
 #     else:
 #         turn_left()
-
+#
 # DAY 7 ----------------------------------------------------------------------------------------------------------------
+word_list = ["bank"]
+random.shuffle(word_list)
+chosen_word = word_list[0]
+incorrect_count = 0
+correct_count = 0
+
+for checking in itertools.count():
+    guess = input("Enter a letter as your guess: ")
+    if guess in chosen_word:
+        print(f"Correct! The letter {guess} is in the word.")
+        correct_count += 1
+        if correct_count == len(chosen_word):
+            print(f"Congratulations! You've guessed the word: {chosen_word}")
+            break
+        else:
+            continue
+    else:
+        print(f"Wrong! The letter {guess} is not in the word.")
+        incorrect_count += 1
+
+        if incorrect_count == 6:
+            print("Game Over, You Lose!")
+            break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
