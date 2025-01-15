d54f92ddd64605534b27535cf1dc6e7e36d536c8
Efe <efeserdar123123@gmail.com>
2025-01-15 18:46:05 +0300

    day6.

--- main.py
+++ main.py
@@ -259,65 +259,120 @@ import random
 
 # DAY 5 ----------------------------------------------------------------------------------------------------------------
 
-fruits = ["Apple", "Peach", "Pear"]
-
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " " + "Pie")
-    print(fruits)
-
-student_scores = [10,50,1000,5000]
-
-total_exam_score = sum(student_scores)
-
-sum = 0
-for score in student_scores:
-    sum += score
-
-print(sum)
-
-print(max(student_scores))
-
-student_result = [13,96,1313,9696]
-max_score = 0
-for score in student_result:
-    if score > max_score:
-        max_score = score
-
-for number in range(30,70,3): # start-stop-step mechanism
-    print(number)
-
-total = 0
-for number in range(0,101,1):
-    total += number # it just skips to the next line when the loop ends.
-print(total)
-
-letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-           "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", "\"", ",", ".", "/", "<", ">", "?"]
-
-lenght_password = int((input("How long do you want your password to be?\n")))
-symbols_password = int(input("How many symbols do you want in your password to be?\n"))
-numbers_password = int(input("How many numbers do you want in your password to be?\n"))
-
-password = []
-
-for length in range(0,lenght_password):
-    random_letter = random.choice(letters)
-    password += random_letter
-
-for number in range(0,numbers_password):
-    random_number = random.choice(numbers)
-    password += random_number
-
-for symbol in range(0,symbols_password):
-    random_symbol = random.choice(symbols)
-    password += random_symbol
-
-random.shuffle(password)
-print((password))
+# fruits = ["Apple", "Peach", "Pear"]
+#
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + " " + "Pie")
+#     print(fruits)
+#
+# student_scores = [10,50,1000,5000]
+#
+# total_exam_score = sum(student_scores)
+#
+# sum = 0
+# for score in student_scores:
+#     sum += score
+#
+# print(sum)
+#
+# print(max(student_scores))
+#
+# student_result = [13,96,1313,9696]
+# max_score = 0
+# for score in student_result:
+#     if score > max_score:
+#         max_score = score
+#
+# for number in range(30,70,3): # start-stop-step mechanism
+#     print(number)
+#
+# total = 0
+# for number in range(0,101,1):
+#     total += number # it just skips to the next line when the loop ends.
+# print(total)
+#
+# letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+#            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+# numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+#
+# symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", "\"", ",", ".", "/", "<", ">", "?"]
+#
+# lenght_password = int((input("How long do you want your password to be?\n")))
+# symbols_password = int(input("How many symbols do you want in your password to be?\n"))
+# numbers_password = int(input("How many numbers do you want in your password to be?\n"))
+#
+# password = []
+#
+# for length in range(0,lenght_password):
+#     random_letter = random.choice(letters)
+#     password += random_letter
+#
+# for number in range(0,numbers_password):
+#     random_number = random.choice(numbers)
+#     password += random_number
+#
+# for symbol in range(0,symbols_password):
+#     random_symbol = random.choice(symbols)
+#     password += random_symbol
+#
+# random.shuffle(password)
+# print((password))
 
 # DAY 6 ----------------------------------------------------------------------------------------------------------------
+#
+#  def turn_right():
+#     turn_left
+#     turn_left
+#     turn_left
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left
+#
+# for step in range(6)
+#     jump()
+#
+# number_of_hurdles =6
+# while(number_of_hurdles > 0):
+#     jump()
+#     number_of_hurdles -= 1
+#     print(number_of_hurdles)
+# while not at_goal():
+#     jump()
+# def jump():
+# turn_left()
+# while wall_on_right():
+#     move()
+# turn_right()
+# move()
+# turn_right()
+# while front_is_clear():
+#     move()
+# turn_left()
+#
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+# while front_is_clear():
+#     move()
+# turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
 
+# DAY 7 ----------------------------------------------------------------------------------------------------------------
