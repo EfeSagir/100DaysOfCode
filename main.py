574904053abe1215325f148531823ec5961a54b9
Efe <efeserdar123123@gmail.com>
2025-02-02 15:13:26 +0300

    day9finished.

--- main.py
+++ main.py
@@ -614,69 +614,88 @@ import itertools
 #     print("Okay bro.")
 
 # DAY 9 ----------------------------------------------------------------------------------------------------------------
-
-student_scores = {
-    'Harry': 88,
-    'Ron': 78,
-    'Hermione': 95,
-    'Draco': 75,
-    'Neville': 60
-}
-
-student_grades = {}
-for key in student_scores:
-    score = student_scores[key]
-    if score >= 91:
-        student_grades[key] = "Outstanding"
-    elif score >= 81:
-        student_grades[key] = "Exceeds Expectations"
-    elif score >= 71:
-        student_grades[key] = "Acceptable"
-    else:
-        student_grades[key] = "Fail"
-
-print(student_grades)
-
-capitals = {
-    "France": "Paris",
-    "Germany": "Berlin"
-}
-
-print(capitals.get("France"))
-capitals.update({"Turkey": "Istanbul"})
-print(capitals)
-capitals.pop("Germany")
-capitals.popitem()
-capitals.clear
-keys = capitals.keys()
-print(keys)
-values = capitals.values()
-print(values)
-items = capitals.items()
-print(items)
-
-for key,value in items:
-    print(f"{key} : {value}")
-
-# travel_log = {
-#     "France": ["Paris", "Lille", "Dij"],
-#     "Germany": ["Stuttgart", "Berlin"]
+#
+# student_scores = {
+#     'Harry': 88,
+#     'Ron': 78,
+#     'Hermione': 95,
+#     'Draco': 75,
+#     'Neville': 60
 # }
+#
+# student_grades = {}
+# for key in student_scores:
+#     score = student_scores[key]
+#     if score >= 91:
+#         student_grades[key] = "Outstanding"
+#     elif score >= 81:
+#         student_grades[key] = "Exceeds Expectations"
+#     elif score >= 71:
+#         student_grades[key] = "Acceptable"
+#     else:
+#         student_grades[key] = "Fail"
+#
+# print(student_grades)
+#
+# capitals = {
+#     "France": "Paris",
+#     "Germany": "Berlin"
+# }
+#
+# print(capitals.get("France"))
+# capitals.update({"Turkey": "Istanbul"})
+# print(capitals)
+# capitals.pop("Germany")
+# capitals.popitem()
+# capitals.clear
+# keys = capitals.keys()
+# print(keys)
+# values = capitals.values()
+# print(values)
+# items = capitals.items()
+# print(items)
+#
+# for key,value in items:
+#     print(f"{key} : {value}")
+#
+# # travel_log = {
+# #     "France": ["Paris", "Lille", "Dij"],
+# #     "Germany": ["Stuttgart", "Berlin"]
+# # }
+#
+# # print(travel_log["France"][1])
+#
+# nested_list = ["A","B",["C","D"]]
+# # print(nested_list[2][1])
+#
+# travel_log = {
+#     "France": {
+#         "cities_visited": ["Paris", "Lille", "Dijon"],
+#         "num_times_visited": 12
+#     },
+#     "Germany": {
+#         "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+#         "num_times_visited": 5
+#     }
+# }
+#
+# print(travel_log["Germany"]["cities_visited"][2])
+#
+# # operator = input("pls enter operator")
+# #
+# # if isinstance(operator, str):
+# #     print(operator)
 
-# print(travel_log["France"][1])
+Dictionary = {"Key" : "Value"}
 
-nested_list = ["A","B",["C","D"]]
-# print(nested_list[2][1])
+programming_dictionary = {"Bug" : "An error in a program that prevents the program from running as expected.",
+                          "Function" : "A piece of code that you can easily call over and over again.",
+                          "Loop" : "The action of doing something of doing over and over again.",
+                          123 : "It is a combination of the first 3 positive numbers"}
 
-travel_log = {
-    "France": {
-        "cities_visited": ["Paris", "Lille", "Dijon"],
-        "num_times_visited": 12
-    },
-    "Germany": {
-        "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
-        "num_times_visited": 5
-    }
-}
-
-print(travel_log["Germany"]["cities_visited"][2])
+print(programming_dictionary["Bug"])
+print(programming_dictionary["Function"])
+print(programming_dictionary["Loop"])
+print(programming_dictionary[123])
+programming_dictionary["Continue"] = "It is a control flow statament for loops to whether you want to skip execution of a specific part in your code."
+print(programming_dictionary["Continue"])
