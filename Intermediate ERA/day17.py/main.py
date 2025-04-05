92a770ab72cfa914ad19bfad59fd5a7fdd742e93
Efe <efeserdar123123@gmail.com>
2025-04-06 00:35:30 +0300

    Intermediate ERA

--- Intermediate ERA/day17.py/main.py
+++ Intermediate ERA/day17.py/main.py
@@ -4,18 +4,17 @@ from quiz_brain import QuizBrain
 question_bank = []
 point = 0
 QuestionNumber = 0
-
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
-    question_object = Question(question_text, question_answer)
-    question_bank.append(question_object)
+for data in question_data:
+    for question in data["results"]:
+        question_text = question["question"]
+        question_answer = question["correct_answer"]
+        question_object = Question(question_text, question_answer)
+        question_bank.append(question_object)
 
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     user_answer, correct_answer = quiz.next_question()
-    quiz.next_question()
     QuestionNumber += 1
     if user_answer == correct_answer.lower():
         point += 1
