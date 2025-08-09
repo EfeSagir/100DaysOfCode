ca27b3a6bde6818dee62dc8d603ffb62372eff2d
Efe <efeserdar123123@gmail.com>
2025-08-09 15:43:29 +0300

    Intermediate ERA

--- Intermediate ERA/day18.py
+++ Intermediate ERA/day18.py
@@ -1,46 +1,43 @@
-from turtle import Turtle as a 
-from turtle import Screen as b
-import heroes
+import turtle as t
 import random
-print(heroes.gen())
+t.Turtle()
 
-TimmyTheTurtle = a()
+color_list = [(236, 248, 243), (36, 95, 183), (236, 165, 79), (244, 223, 87), (215, 69, 105), (98, 197, 234),
+              (250, 51, 22), (203, 70, 21), (240, 106, 143), (185, 47, 90), (143, 233, 216), (252, 136, 166),
+              (165, 175, 233), (66, 45, 13), (72, 205, 170), (83, 187, 100), (20, 156, 51), (24, 36, 86), (252, 220, 0),
+              (164, 28, 8), (105, 39, 44), (250, 152, 2), (22, 151, 229), (108, 213, 249), (254, 12, 3), (38, 48, 98),
+              (98, 96, 186)]
 
-TimmyTheTurtle.shape("turtle")
-TimmyTheTurtle.color("green")
+random_color = (random.choice(color_list))
+t.colormode(255)
+t.penup()
+t.speed("fastest")
+t.hideturtle()
 
 
-def MakeSquare():
-    TimmyTheTurtle.right(90)
+def left_up():
+    t.setheading(90)
+    t.forward(50)
+    t.setheading(0)
 
 
-# for i in range(15):
-#     TimmyTheTurtle.forward(10)
-#     TimmyTheTurtle.penup()
-#     TimmyTheTurtle.forward(10)
-#     TimmyTheTurtle.pendown()
+def right_up():
+    t.setheading(90)
+    t.forward(50)
+    t.setheading(180)
 
-        
-colors = [
-    "red", "blue", "green", "orange", "purple",
-    "yellow", "pink", "brown", "cyan", "magenta",
-    "lime", "gold", "turquoise", "violet", "gray"
-]
 
-for SidesNumber in range(3,11):
-    for i in range(SidesNumber):
-        TimmyTheTurtle.forward(100)
-        TimmyTheTurtle.right(360/(SidesNumber))
-        TimmyTheTurtle.color(random.choice(colors))
-    
-# MakeSquare()
-# MakeSquare()
-# MakeSquare()
-# MakeSquare()
+for _ in range(5):
+    for _ in range(10):
+        t.dot(20, (random.choice(color_list)))
+        t.setheading(180)
+        t.forward(50)
+    left_up()
+    for _ in range(10):
+        t.forward(50)
+        t.dot(20, (random.choice(color_list)))
+    right_up()
 
-# for i in range(4):
-#     TimmyTheTurtle.forward(100)
-#     TimmyTheTurtle.right(90)
 
-Screen = b()
-Screen.exitonclick()
+screen = t.Screen()
+screen.exitonclick()
---
+++ Intermediate ERA/day19.py
@@ -0,0 +1,54 @@
+from turtle import Turtle,Screen
+import random
+is_race_on = None
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+all_turtles = []
+for i in range(6):
+    t = Turtle()
+    t.color(colors[i])
+    t.shape("turtle")
+    t.penup()
+    t.goto(-300,200 -(i*50))
+    t.pendown()
+    all_turtles.append(t)
+
+
+
+screen = Screen()
+user_bet = screen.textinput("MAKE YOUR GUESS", "WHICH TURTLE WILL WIN?")
+screen.setup(600,500)
+
+
+def Forwards():
+    Turtle_One.fd(10)
+def Backwards():
+    Turtle_One.bd(10)
+def Counter_Clockwise():
+    Turtle_One.left(10)
+def Clockwise():
+    Turtle_One.right(10)
+def Clear():
+    Turtle_One.clear()
+    Turtle_One.penup()
+    Turtle_One.home()
+    Turtle_One.pendown()
+
+
+if user_bet:
+    is_race_on = True
+while is_race_on:   
+    
+    for turtle in all_turtles:
+        turtle.penup()
+        rand_distance = random.randint(0,10)
+        turtle.fd(rand_distance)
+        if turtle.xcor() > 230:
+            print(turtle.color())
+            is_race_on = False
+            if(str(user_bet) == turtle.color()):
+                print("YOU WON")
+            else:
+                print("YOU LOSE")
+screen.exitonclick()
+        
